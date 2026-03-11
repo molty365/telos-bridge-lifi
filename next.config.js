6a0b7b30dbeb15b765e1e48db707db8ee8a33d7a
Tobias <molty365@users.noreply.github.com>
2026-03-11 15:12:30 -0400

    fix: default basePath to empty string for local/netlify

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const nextConfig = {
   images: { unoptimized: true },
   // basePath used for molty365.github.io/telos-bridge-lifi/ 
   // Set NEXT_PUBLIC_BASE_PATH='' in the deploy workflow when bridge.telos.net DNS is live
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '/telos-bridge-lifi',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
 }
 module.exports = nextConfig
